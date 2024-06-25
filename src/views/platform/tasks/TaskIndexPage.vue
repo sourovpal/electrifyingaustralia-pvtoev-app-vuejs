e525ba9f43dbbdbbb5c1d3757bc689fc37e3beab
fahim <powermanfahim@gmail.com>
2024-06-25 07:54:34 +0600

    Using the custom svg to simulate a checkbox

--- src/views/platform/tasks/TaskIndexPage.vue
+++ src/views/platform/tasks/TaskIndexPage.vue
@@ -5,16 +5,21 @@ import DropdownUsersList from './DropdownUsersList.vue';
 import { onMounted, ref } from 'vue';
 import { Tooltip } from 'bootstrap';
 import axios from '../../../actions/api.js';
+  import {useToast} from 'vue-toast-notification';
+import { CONFIG } from '../../../config.js';
+
 
 onMounted(() => {
     getWorkFlows();
     getUsers();
 })
 
+const $toast = useToast(CONFIG.TOAST);
+
 const workflows = ref([]);
 async function getWorkFlows() {
     axios.get('workflows').then(res => {
-        workflows.value = res.data
+        workflows.value = res.data.map(workflow => ({...workflow, tasks: workflow.tasks.map(task => ({...task, is_complete: false}))}));
     }).catch(() => {
         $toast.error('Something went wrong');
     })
@@ -24,6 +29,14 @@ async function getWorkFlows() {
             .map(tooltipTriggerEl => new Tooltip(tooltipTriggerEl));
     });
 }
+
+const handleTaskClick = (workflowId, taskId) => {
+    const workflow = workflows.value.find(workflow => workflow.id === workflowId);
+    if (!workflow) return;
+    const task = workflow.tasks.find(task => task.id === taskId);
+    task.is_complete = !task.is_complete;
+}
+
 
 const users = ref([]);
 async function getUsers() {
@@ -61,7 +74,7 @@ const leadStatus = [
         <action-bar>
             <div 
                 v-if="true"         
-                class="settings-group-item owner-list-dropdown ms-3 ps-2 position-relative d-none d-xl-block">
+                class="settings-group-item owner-list-dropdown ms-3 ps-2 d-none d-xl-block">
                 <button 
                     class="user-dropdown-toggler bg-transparent border-0" 
                     data-mdb-toggle="dropdown" 
@@ -91,7 +104,25 @@ const leadStatus = [
                     <div class="task-bar shadow border" v-for="workflow in workflows">
                         <div class="row-1 d-flex align-items-center justify-content-between mb-4">
                             <div class="d-flex gap-2 align-items-center">
-                                <input type="checkbox" />
+                                <!-- <input type="checkbox" /> -->
+                                <label 
+                                    data-bs-toggle="tooltip" 
+                                    data-bs-placement="top" 
+                                    title="beans" 
+                                    @click="handleTaskClick(null, null)" 
+                                    type="checkbox" 
+                                    class="custom-form-checkbox"
+                                >
+                                    <svg
+                                        v-if="true"
+                                        class="unchecked"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="currentColor"
+                                        height="24"
+                                        viewBox="0 -960 960 960" 
+                                        width="24">
+                                        <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Z"/></svg>
+                                </label>
                                 <p class="fw-bold mb-0">{{ workflow.title }}</p>
                                 <small class="status border fw-bold rounded px-2 py-1">Status</small>
                             </div>
@@ -99,10 +130,36 @@ const leadStatus = [
                         </div>
 
                         <div class="row-2 d-flex justify-content-between align-items-end">
-                            <div class="checkboxes-wrapper d-flex flex-wrap gap-2">
-                                <input v-for="task in workflow.tasks" data-bs-toggle="tooltip" data-bs-placement="top" :title="task.title" type="checkbox"  />
-                                <!-- <input data-bs-toggle="tooltip" data-bs-placement="top" title="beans bitch 2!" type="checkbox"  /> -->
-                                <!-- <input checked data-bs-toggle="tooltip" data-bs-placement="top" title="beans bitch!" type="checkbox" /> -->
+                            <div class="checkboxes-wrapper d-flex flex-wrap --gap-2">
+                                <label 
+                                    v-for="task in workflow.tasks" 
+                                    data-bs-toggle="tooltip" 
+                                    data-bs-placement="top" 
+                                    :title="task.title" 
+                                    type="checkbox" 
+                                    @click="handleTaskClick(workflow.id, task.id)" 
+                                    class="custom-form-checkbox"
+                                >
+                                    <svg
+                                        v-if="!task.is_complete"
+                                        class="unchecked"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="currentColor"
+                                        height="18"
+                                        viewBox="0 -960 960 960" 
+                                        width="18">
+                                            <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Z"/></svg>
+                                    <svg
+                                        v-else
+                                        class="checked"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="currentColor"
+                                        width="18"
+                                        height="18"
+                                        viewBox="0 0 24 24">
+                                            <path d="M19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.11 0 2-.9 2-2V5c0-1.1-.89-2-2-2zm-9 14l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"></path>
+                                    </svg>
+                                </label>
                             </div>
 
                             <div class="task-actions d-flex align-items-center">

5cde2bab2c2c352c8761893756cd76f193572fbe
fahim <powermanfahim@gmail.com>
2024-06-28 14:26:09 +0600

    Finance UI wip

--- src/views/library/LibraryComponents/Financing.vue
+++ src/views/library/LibraryComponents/Financing.vue
@@ -1,13 +1,61 @@
 <template>
-    <div>
-        <p class="display-3">Financing</p>
-    </div>
+	<div>
+		<div class="row align-items-center mt-4">
+			<p class="text-end fs-14px col-md-4 text-secondary">
+				Total amount
+			</p>
+			<div class="col-md-8">
+				<div class="d-flex gap-3 align-items-center">
+					<p class="fs-14px mb-0 text-black">$27,990.00</p>
+					<font-awesome-icon
+						class="text-secondary fs-14px"
+						icon="fas fa-pen"
+					/>
+					<p class="text-info mb-0 fs-12px fw-bold">
+						Use project total price
+					</p>
+				</div>
+				<p class="fs-12px">
+					What is the total cash amount of the project? Incl. GST
+				</p>
+			</div>
+		</div>
+
+		<div class="row align-items-center">
+			<p class="text-end fs-14px col-md-4 text-secondary">Deposit</p>
+			<div class="col-md-8">
+				<div class="d-flex gap-3 align-items-center">
+					<p class="fs-14px mb-0 text-black">-</p>
+					<font-awesome-icon
+						class="text-secondary fs-14px"
+						icon="fas fa-pen"
+					/>
+					<p class="text-info mb-0 fs-12px fw-bold">
+						Use project total price
+					</p>
+				</div>
+				<p class="fs-12px">
+					How much does the customer need to finance?
+				</p>
+			</div>
+		</div>
+
+		<div class="row align-items-center">
+			<p class="text-end fs-14px col-md-4 text-secondary">
+				Financed Amount
+			</p>
+			<div class="col-md-8">
+				<div class="d-flex gap-3 align-items-center">
+					<p class="fs-14px mb-0 text-black">$27,990.00</p>
+				</div>
+				<p class="fs-12px">
+					How much does the customer need to finance?
+				</p>
+			</div>
+		</div>
+	</div>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>

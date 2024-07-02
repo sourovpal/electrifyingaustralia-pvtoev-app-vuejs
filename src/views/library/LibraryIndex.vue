ed3862fd8799076e52efa7ef3f367703bda035c7
fahim <powermanfahim@gmail.com>
2024-07-02 09:00:42 +0600

    Fixed the proposal list in the proposal view

--- src/views/library/LibraryIndex.vue
+++ src/views/library/LibraryIndex.vue
@@ -1,14 +1,13 @@
 <script setup>
 import MobileHeader from '../../components/MobileHeader.vue';
-import Menu from '../../components/Menu/Menu.vue';
-import {title, menus} from './menu';
+import LibraryMenu from './LibraryMenu.vue';
  
 </script>
 
 <template>
     <MobileHeader/>
     <section class="d-flex flex-row">
-        <Menu :menus="menus" :title="title" />
+        <LibraryMenu />
         <router-view></router-view>
     </section>
 </template>

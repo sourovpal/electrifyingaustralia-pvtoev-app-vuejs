6517ead690e9a3f231c690f85db75b7875fc2f5e
fahim <powermanfahim@gmail.com>
2024-08-08 14:28:41 +0600

    Handling more edge cases

--- src/views/library/LibraryComponents/AddPricingInput.vue
+++ src/views/library/LibraryComponents/AddPricingInput.vue
@@ -16,8 +16,9 @@
         <div class="col-md-2">
             <input
                 placeholder="Quantity"
-                v-model="formData.quantity"
-                class="form-control text-end p-1"
+                :min="0"
+                v-model.number="formData.quantity"
+                :class="`form-control text-end p-1 ${ formData.quantity < 0 ? 'invalid-input' : '' }`"
                 type="number"
             />
         </div>
@@ -25,15 +26,16 @@
         <div class="col-md-2">
             <input
                 placeholder="Unit price"
-                class="form-control text-end p-1"
-                v-model="formData.unit_price"
+                :class="`form-control text-end p-1 ${ formData.unit_price < 0 ? 'invalid-input' : '' }`"
+                :min="0"
+                v-model.number="formData.unit_price"
                 type="number"
             />
         </div>
 
         <div class="col-md-2 d-flex gap-3 justify-content-end pricing-control">
 			<font-awesome-icon
-			    :class="`create-pricing-btn cursor-pointer ${Boolean(formData.description) ? '' : 'disabled'}`"
+			    :class="`create-pricing-btn cursor-pointer ${invalidInput ? 'disabled' : ''}`"
 				icon="fas fa-circle-check"
 			    @click="handleCreateClick"
 			/>
@@ -48,7 +50,7 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import  axios from '../../../actions/api.js';
 import { useRoute } from 'vue-router';
 import UnitSelector from './UnitSelector.vue';
@@ -63,6 +65,21 @@ const formData = ref({
     unit_price: props.pricing?.unit_price ?? '',
     unit_id: props.pricing?.unit_id ?? 1,
 })
+
+const invalidInput = computed(() => {
+    if (!Boolean(formData.value.description)) return true;
+
+    // not just relying on :min attribute due to firefox's incompatibility
+    if (typeof formData.value.quantity === 'number') {
+        if (formData.value.quantity < 0) return true;
+    }
+
+    if (typeof formData.value.unit_price === 'number') {
+        if (formData.value.unit_price < 0) return true;
+    }
+
+    return false;
+});
 
 const handleCreateClick = async () => {
     const projectId = currentRoute.params.project_id;
@@ -96,6 +113,11 @@ $scale-factor: 1.25;
 .pricing-control svg {
     color: #adadad;
     transition: 100ms;
+}
+
+.invalid-input {
+    color: #dc4c64;
+    border: 2px solid;
 }
 
 .create-pricing-btn {
--- src/views/library/LibraryComponents/PricingItem.vue
+++ src/views/library/LibraryComponents/PricingItem.vue
@@ -1,119 +1,176 @@
 <template>
-    <div v-if="!editMode" class="row pricing-value align-items-center px-4 py-2">
-        <!-- Description -->
-        <small class="col-5 col-md-3 cursor-pointer" @click="toggleEditMode">{{ pricing.description }}</small>
+	<div
+		v-if="!editMode"
+		class="row pricing-value align-items-center px-4 py-2 gx-0"
+	>
+		<!-- Description -->
+		<small
+			:class="pricing.item_hidden ? 'opacity-50' : ''"
+			class="col-5 col-md-3 cursor-pointer"
+			@click="toggleEditMode"
+			>{{ pricing.description }}</small
+		>
 
-        <!-- Units -->
-        <small class="col-1 col-md-2 --text-end">
-            <UnitSelector v-model="unit" />
-        </small>
-        
+		<!-- Units -->
+		<small
+			:class="pricing.item_hidden ? 'opacity-50' : ''"
+			class="col-1 col-md-2"
+		>
+			<UnitSelector v-model="unit" />
+		</small>
 
-        <!-- Quantity -->
-        <small class="col-2 col-md-2 text-end cursor-pointer" @click="toggleEditMode">{{ pricing.quantity }}</small>
+		<!-- Quantity -->
+		<small
+			:class="pricing.item_hidden ? 'opacity-50' : ''"
+			class="col-2 col-md-2 text-end cursor-pointer"
+			@click="toggleEditMode"
+			>{{ pricing.quantity ?? '-' }}</small
+		>
 
-        <!-- Unit Price -->
-        <small class="col-2 col-md-2 text-end cursor-pointer" @click="toggleEditMode">{{ pricing.unit_price }}</small>
+		<!-- Unit Price -->
+		<small
+			:class="pricing.item_hidden ? 'opacity-50' : ''"
+			class="col-2 col-md-2 text-end cursor-pointer"
+			@click="toggleEditMode"
+			>{{ pricing.unit_price ?? '-' }}</small
+		>
 
-        <!-- Total -->
-        <small class="col-2 col-md-2 text-end">
-            {{ pricing.total ? '$' + Formatter.toIntlFormat(pricing.total) : '-' }}
-        </small>
+		<!-- Total -->
+		<small
+			:class="{
+			    'opacity-50':  pricing.item_hidden,
+			    'strike-through':  pricing.price_hidden,
+			}" 
+			class="col-2 col-md-2 text-end"
+		>
+			{{
+				pricing.total
+					? '$' + Formatter.toIntlFormat(pricing.total)
+					: '-'
+			}}
+		</small>
 
 		<div class="col-md-1">
-            <div class="dropdown text-end p-0">
-                <div
-                    class="fw-bold cursor-pointer"
-                    style="display: grid; place-items: center"
-                    data-mdb-toggle="dropdown"
-                    aria-expanded="false"
-                >
-			        <font-awesome-icon 
-			            class="fs-14px" 
-			            icon="fas fa-ellipsis-vertical" 
-			        />
-		        </div>
-                <div class="dropdown-menu shadow" aria-labelledby="dropdownMenuLink">
-                    <li class="py-2 px-3 overflow-hidden dropdown-item cursor-pointer d-flex align-items-center justify-content-between px-4" 
-                        @click="handleItemHide"
-                    >
-                        <span>{{ pricing.item_hidden ? 'Show item' : 'Hide item' }}</span>
-			            <font-awesome-icon 
-			                class="fs-12px" 
-			                :icon="`fas ${ !pricing.item_hidden ? 'fa-eye-slash' : 'fa-eye' }`" 
-			            />
-                    </li>
+			<div class="dropdown text-end p-0">
+				<div
+					class="fw-bold cursor-pointer pricing-item-menu"
+					style="display: grid; place-items: center"
+					data-mdb-toggle="dropdown"
+					aria-expanded="false"
+				>
+					<font-awesome-icon
+						class="fs-14px"
+						icon="fas fa-ellipsis-vertical"
+					/>
+				</div>
+				<div
+					class="dropdown-menu shadow"
+					aria-labelledby="dropdownMenuLink"
+				>
+					<li
+						class="py-2 px-3 overflow-hidden dropdown-item cursor-pointer d-flex align-items-center justify-content-between px-4"
+						@click="handleItemHide"
+					>
+						<span>{{
+							pricing.item_hidden
+								? 'Show item'
+								: 'Hide item'
+						}}</span>
+						<font-awesome-icon
+							class="fs-12px"
+							:icon="`fas ${!pricing.item_hidden ? 'fa-eye-slash' : 'fa-eye'}`"
+						/>
+					</li>
 
-                    <li class="py-2 px-3 overflow-hidden dropdown-item cursor-pointer d-flex align-items-center justify-content-between px-4"
-                        @click="handleItemPriceHide"
-                    >
-                        <span>{{ pricing.price_hidden ? 'Show price' : 'Hide price' }}</span>
-                        <div>
-			                <font-awesome-icon 
-			                class="fs-12px" 
-			                icon="fas fa-dollar-sign" 
-			                />
-                        </div>
-                    </li>
+					<li
+						class="py-2 px-3 overflow-hidden dropdown-item cursor-pointer d-flex align-items-center justify-content-between px-4"
+						@click="handleItemPriceHide"
+					>
+						<span>{{
+							pricing.price_hidden
+								? 'Show price'
+								: 'Hide price'
+						}}</span>
+						<div>
+							<font-awesome-icon
+								class="fs-12px"
+								icon="fas fa-dollar-sign"
+							/>
+						</div>
+					</li>
 
-                    <li class="py-2 px-3 overflow-hidden dropdown-item cursor-pointer d-flex align-items-center justify-content-between px-4 text-danger" @click="handleDeleteClick(pricing.id)">
-                        Delete item
-			            <font-awesome-icon 
-			                class="fs-12px" 
-			                icon="fas fa-trash" 
-			            />
-                    </li>
-                </div>
-	        </div>
+					<li
+						class="py-2 px-3 overflow-hidden dropdown-item cursor-pointer d-flex align-items-center justify-content-between px-4 text-danger"
+						@click="handleDeleteClick(pricing.id)"
+					>
+						Delete item
+						<font-awesome-icon
+							class="fs-12px"
+							icon="fas fa-trash"
+						/>
+					</li>
+				</div>
+			</div>
 		</div>
-    </div>
+	</div>
 
-    <AddPricingInput
-        v-else
-        :pricing
-        @updated="handleUpdated"
-        @cancel="toggleEditMode"
-    />
+	<AddPricingInput
+		v-else
+		:pricing
+		@updated="handleUpdated"
+		@cancel="toggleEditMode"
+	/>
 </template>
 
 <script setup>
-import { onMounted, ref, watch } from 'vue';
-import Formatter from '../../../helpers/Formatter';
-import UnitSelector from './UnitSelector.vue';
-import AddPricingInput from './AddPricingInput.vue';
+import { ref, watch } from 'vue'
+import Formatter from '../../../helpers/Formatter'
+import UnitSelector from './UnitSelector.vue'
+import AddPricingInput from './AddPricingInput.vue'
 
-const props = defineProps(['pricing']);
+const props = defineProps(['pricing'])
 const emit = defineEmits([
-    'item-hide',
-    'item-price-hide',
-    'item-delete',
-    'unit-change',
-    'item-update',
-]);
+	'item-hide',
+	'item-price-hide',
+	'item-delete',
+	'unit-change',
+	'item-update',
+])
 
-const unit = ref();
+const unit = ref(props.pricing.unit_id)
 
-onMounted(() => {
-    unit.value = props.pricing.unit_id;
-});
-
-const editMode = ref(false);
-const toggleEditMode = () => editMode.value = !editMode.value;
+const editMode = ref(false)
+const toggleEditMode = () => (editMode.value = !editMode.value)
 const handleUpdated = () => {
-    editMode.value = false;
-    emit('item-updated') 
-};
+	editMode.value = false
+	emit('item-updated')
+}
 
-const handleItemHide = () => emit('item-hide', props.pricing);
-const handleItemPriceHide = () => emit('item-price-hide', props.pricing);
-const handleDeleteClick = () => emit('item-delete', props.pricing.id);
+const handleItemHide = () => emit('item-hide', props.pricing)
+const handleItemPriceHide = () => emit('item-price-hide', props.pricing)
+const handleDeleteClick = () => emit('item-delete', props.pricing.id)
 
-watch(unit, (newValue) => {
-    emit('unit-change', { pricingId: props.pricing.id, new_unit_id: newValue });
-})
+watch(unit, (newValue) => 
+	emit('unit-change', {
+	    pricingId: props.pricing.id,
+	    new_unit_id: newValue
+	})
+)
 
 </script>
 
 <style lang="scss">
 
+.strike-through { text-decoration: line-through; }
+.pricing-value > small { transition: 200ms; }
+
+.pricing-item-menu {
+    opacity: 0.7;
+    transition: 200ms;
+
+    &:hover {
+        opacity: 1;
+    }
+
+}
 </style>

2f38f924a757a161500b64465a72d482536c3fe3
sourovpal <sourovpal35@gmail.com>
2024-10-30 14:36:28 +0600

    Modify Lead Add Modal

--- src/views/platform/leads/components/modals/AddNewLeadModal.vue
+++ src/views/platform/leads/components/modals/AddNewLeadModal.vue
@@ -1,9 +1,10 @@
 <script setup>
-  import { ref, reactive, watch, computed, onMounted } from "vue";
-  import { usePlatformStore } from "@stores";
+  import { ref, reactive, watch, computed, onMounted, nextTick } from "vue";
+  import { formatLeadAddress } from "@helpers";
   import { useApiRequest } from "@actions";
   import { $toast } from "@config";
-  import { useAuthStore, useLeadsStore } from "@stores";
+  import { useAuthStore, useLeadsStore, usePlatformStore } from "@stores";
+  import { useDebounceFn } from '@vueuse/core';
 
   const emits = defineEmits(["refresh"]);
   const platformStore = usePlatformStore();
@@ -16,11 +17,13 @@
 
   const attributes = reactive({
     name: null,
-    address: null,
-    phone_or_email: null,
+    location: null,
+    phone_number: null,
+    email: null,
     lead_source: null,
     lead_title: null,
     lead_status: null,
+    select_status: null,
     lead_owner: null,
   });
 
@@ -28,12 +31,19 @@
   const isLoading = ref(false);
   const isLoadingUsers = ref(false);
   const leadOwner = ref(null);
-  const filterSources = ref(["dhaka"]);
+  const filterSources = ref([]);
+  const selectedAddress = ref(null);
+
+  const filterContacts = ref([]);
+  const filterAddress = ref({
+    label: 'Existing deals',
+    items: [],
+  });
 
   watch(
     leadStatuses,
     (statuses) => {
-      if (statuses && statuses?.length) attributes.lead_status = statuses[0];
+      if (statuses && statuses?.length) attributes.select_status = statuses[0];
     },
     { immediate: true, deep: true }
   );
@@ -46,54 +56,182 @@
       .map((item) => item.title);
   }
 
+  const handleSearchAddress = useDebounceFn(async ({ query }) => {
+    selectedAddress.value = null;
+    await useApiRequest({
+      url: '/platform/search/leads',
+      method: 'post',
+      payload: {
+        limit: 5,
+        search: query,
+        lead_columns: ['address_line_one', 'address_line_two', 'city', 'state', 'country'],
+        contact: true,
+      }
+    }).then(leads => {
+      if (!leads && !leads.length) filterAddress.value = { ...filterAddress.value, items: [] };
+
+      filterAddress.value = {
+        ...filterAddress.value,
+        items: leads
+      };
+
+    }).catch(error => {
+      filterAddress.value = { ...filterAddress.value, items: [] }
+    });
+
+  }, 500);
+
+  const handleSearchContact = useDebounceFn(async ({ query }) => {
+
+    await useApiRequest({
+      url: '/platform/search/leads',
+      method: 'post',
+      payload: {
+        limit: 10,
+        search: query,
+        contact_columns: ['first_name', 'last_name', 'email', 'phone_number'],
+      }
+    }).then(contacts => {
+
+      if (!contacts && !contacts.length) filterContacts.value = [];
+
+      filterContacts.value = contacts;
+
+    }).catch(error => {
+      filterContacts.value = []
+    });
+
+  }, 500);
+
   function hideModal() {
     leadsStore.setAddNewLeadModal(false);
   }
 
   function fetchUsers() {
+
     if (platformStore.getUsers.length) return;
+
     platformStore.callFetchUsers(({ loading }) => {
       isLoadingUsers.value = loading;
     });
+
   }
 
   async function handleCreateNewLead() {
     errors.value = {};
     isLoading.value = true;
 
+    if (selectedAddress.value) {
+
+      attributes["address"] = selectedAddress.value;
+
+    } else if (attributes.location) {
+
+      attributes["address"] = { address_one: attributes.location };
+
+    }
+
+    if (attributes.select_status && attributes.select_status?.status_id)
+      attributes["lead_status"] = attributes.select_status?.status_id;
+
     attributes["lead_owner"] = leadOwner.value?.user_id;
 
     await useApiRequest({
-      url: "/leads",
+      url: "/platform/leads",
       method: "POST",
-      payload: { ...attributes, lead_status: attributes.lead_status?.status_id },
+      payload: attributes,
     })
       .then((res) => {
+
         const { success, message, errors: isErrors } = res;
+
         if (success) {
+
           $toast[message.type](message.text);
+
           hideModal();
+
           return emits("refresh", true);
+
         }
+
         errors.value = isErrors;
+
       })
       .catch((error) => {
-        $toast.error("Oops, something went wrong");
+
+        $toast.error(error.message);
+
       })
       .finally(() => {
+
         isLoading.value = false;
+
       });
   }
 
   onMounted(() => {
     leadOwner.value = authUser.value ?? {};
   });
+
+
+  function handleSelectAddress(option) {
+    selectedAddress.value = null;
+    attributes.location = formatLeadAddress(option.value)
+    selectedAddress.value = {
+      address_one: option.value?.address_line_one,
+      address_two: option.value?.address_line_two,
+      city: option.value?.city,
+      state: option.value?.state,
+      post_code: option.value?.post_code,
+    };
+  }
+
+  function handleSelectPhoneNumber(option) {
+    nextTick(() => {
+      if (option.value.email && !attributes.email) attributes.email = option.value.email;
+
+      if (option.value.full_name && !attributes.name) attributes.name = option.value.full_name;
+
+      if (!option.value.phone_number) return attributes.phone_number = null;
+      attributes.phone_number = option.value.phone_number;
+    });
+  }
+
+  function handleSelectEmailAddress(option) {
+
+    nextTick(() => {
+      if (option.value.phone_number && !attributes.phone_number) attributes.phone_number = option.value.phone_number;
+
+      if (option.value.full_name && !attributes.name) attributes.name = option.value.full_name;
+
+      if (!option.value.email) return attributes.email = null;
+      attributes.email = option.value.email;
+    });
+
+  }
+
+  function handleSelectCustomerName(option) {
+    nextTick(() => {
+
+      if (option.value.email && !attributes.email) attributes.email = option.value.email;
+
+      if (option.value.phone_number && !attributes.phone_number) attributes.phone_number = option.value.phone_number;
+
+      if (!option.value.full_name) return attributes.name = null;
+
+      attributes.name = option.value.full_name;
+
+    });
+  }
+
+
 </script>
 
 <template>
   <modal-dialog modal
     v-bind="$attrs"
-    pt:root:class="!border-0 !bg-transparent overflow-hidden"
+    pt:root:class="rounded-3 overflow-hidden"
     pt:mask:class="backdrop-blur-sm"
     :style="{ width: '35vw' }">
     <template #container="{ closeCallback }">
@@ -120,12 +258,42 @@
               </label>
 
               <icon-field>
-                <input-icon class="pi pi-user" />
-                <input-text size="small"
-                  class="w-100"
+
+                <input-icon class="pi pi-user z-index-999" />
+                <auto-complete class="w-100 auto-complete-small"
+                  panel-class="auto-complete-option-small"
+                  input-class="w-100 auto-complete-input"
+                  dropdown-class="py-1"
+                  placeholder="Full name"
+                  optionLabel="full_name"
                   @focus="delete errors?.name"
                   v-model="attributes.name"
-                  placeholder="Full name" />
+                  :suggestions="filterContacts"
+                  @option-select="handleSelectCustomerName"
+                  @complete="handleSearchContact">
+
+                  <template #option="slotProps">
+                    <div class="d-block">
+                      <div class="d-flex justify-context-start align-items-center">
+                        <div class="circle-avatar me-2"
+                          style="--height: 1.6rem; --width: 1.6rem">
+                          <img :alt="slotProps.option?.full_name"
+                            :src="slotProps.option?.avatar"
+                            class="avatar" />
+                        </div>
+                        <div class="owner-name fs-12px text-hard">
+                          <div class="">
+                            {{ slotProps.option?.full_name }}
+                          </div>
+                          <div class="text-soft fs-12px">
+                            {{ slotProps.option?.email }} {{ slotProps.option?.phone_number }}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </template>
+
+                </auto-complete>
               </icon-field>
 
               <span class="fs-14px text-danger py-1 w-100 d-block"
@@ -141,63 +309,150 @@
               </label>
 
               <icon-field>
-                <input-icon class="pi pi-home" />
-                <input-text class="w-100"
-                  size="small"
+                <input-icon class="pi pi-home z-index-999" />
+                <auto-complete class="w-100 auto-complete-small"
+                  panel-class="auto-complete-option-small"
+                  input-class="w-100 auto-complete-input"
+                  dropdown-class="py-1"
+                  placeholder="Address"
+                  optionGroupLabel="label"
+                  optionLabel="address_line_one"
+                  optionGroupChildren="items"
                   @focus="delete errors?.address"
-                  v-model="attributes.address"
-                  placeholder="Address" />
+                  v-model="attributes.location"
+                  :suggestions="[filterAddress]"
+                  @option-select="handleSelectAddress"
+                  @complete="handleSearchAddress">
+
+                  <template #optiongroup="{option}">
+                    <div class="flex items-center">
+                      <div class="fs-14px">{{ option.label }}</div>
+                    </div>
+                  </template>
+
+                  <template #option="{option}">
+                    <div class="flex items-center">
+                      <div>{{ formatLeadAddress(option) }}</div>
+                      <div class="d-flex justify-context-start align-items-center fs-12px text-soft">
+                        <span class="me-2">{{ option.primary_contact?.full_name }}</span>
+                        <span class="me-2">{{ option.primary_contact?.phone }}</span>
+                        <span class="me-2">{{ option.primary_contact?.email }}</span>
+                      </div>
+                    </div>
+                  </template>
+
+                </auto-complete>
               </icon-field>
 
               <span class="fs-14px text-danger py-1 w-100 d-block"
                 v-if="errors?.address?.length">
                 {{ errors?.address[0] }}
               </span>
+
             </div>
 
             <div class="mb-3">
               <label class="mb-2 fs-16px text-head">
-                Phone or email
+                Phone number
                 <span class="text-soft fs-12px ms-1">(Optional)</span>
               </label>
 
               <icon-field>
-                <input-icon class="pi pi-address-book" />
-                <input-text class="w-100"
-                  size="small"
-                  @focus="delete errors?.phone_or_email"
-                  v-model="attributes.phone_or_email"
-                  placeholder="Phone or email address" />
-              </icon-field>
-
-              <span class="fs-14px text-danger py-1 w-100 d-block"
-                v-if="errors?.phone_or_email?.length">
-                {{ errors?.phone_or_email[0] }}
-              </span>
-            </div>
-
-            <div class="mb-3">
-              <label class="mb-2 fs-16px text-head">
-                Source
-                <span class="text-soft fs-12px ms-1">(Optional)</span>
-              </label>
-
-              <icon-field>
-                <input-icon class="pi pi-pen-to-square z-index-999" />
+                <input-icon class="pi pi-address-book z-index-999" />
                 <auto-complete class="w-100 auto-complete-small"
                   panel-class="auto-complete-option-small"
-                  input-class="w-100 input-lead-source"
+                  input-class="w-100 auto-complete-input"
                   dropdown-class="py-1"
-                  placeholder="Type lead source"
-                  @focus="delete errors?.lead_source"
-                  v-model="attributes.lead_source"
-                  :suggestions="filterSources"
-                  @complete="handleFilterSource" />
+                  placeholder="Phone number"
+                  optionLabel="phone_number"
+                  @focus="delete errors?.phone_number"
+                  v-model="attributes.phone_number"
+                  :suggestions="filterContacts"
+                  @option-select="handleSelectPhoneNumber"
+                  @complete="handleSearchContact">
+
+                  <template #option="slotProps">
+                    <div class="d-block">
+                      <div class="d-flex justify-context-start align-items-center">
+                        <div class="circle-avatar me-2"
+                          style="--height: 1.6rem; --width: 1.6rem">
+                          <img :alt="slotProps.option?.full_name"
+                            :src="slotProps.option?.avatar"
+                            class="avatar" />
+                        </div>
+                        <div class="owner-name fs-12px text-hard">
+                          <div class="">
+                            {{ slotProps.option?.full_name }}
+                          </div>
+                          <div class="text-soft fs-12px">
+                            {{ slotProps.option?.email }} {{ slotProps.option?.phone_number }}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </template>
+
+                </auto-complete>
               </icon-field>
 
               <span class="fs-14px text-danger py-1 w-100 d-block"
-                v-if="errors?.lead_source?.length">{{ errors?.lead_source[0] }}</span>
+                v-if="errors?.phone_number?.length">
+                {{ errors?.phone_number[0] }}
+              </span>
+
             </div>
+
+            <div class="mb-3">
+              <label class="mb-2 fs-16px text-head">
+                Email address
+                <span class="text-soft fs-12px ms-1">(Optional)</span>
+              </label>
+
+              <icon-field>
+                <input-icon class="pi pi-envelope z-index-999" />
+                <auto-complete class="w-100 auto-complete-small"
+                  panel-class="auto-complete-option-small"
+                  input-class="w-100 auto-complete-input"
+                  dropdown-class="py-1"
+                  placeholder="Email address"
+                  optionLabel="email"
+                  @focus="delete errors?.email"
+                  v-model="attributes.email"
+                  :suggestions="filterContacts"
+                  @option-select="handleSelectEmailAddress"
+                  @complete="handleSearchContact">
+
+                  <template #option="slotProps">
+                    <div class="d-block">
+                      <div class="d-flex justify-context-start align-items-center">
+                        <div class="circle-avatar me-2"
+                          style="--height: 1.6rem; --width: 1.6rem">
+                          <img :alt="slotProps.option?.full_name"
+                            :src="slotProps.option?.avatar"
+                            class="avatar" />
+                        </div>
+                        <div class="owner-name fs-12px text-hard">
+                          <div class="">
+                            {{ slotProps.option?.full_name }}
+                          </div>
+                          <div class="text-soft fs-12px">
+                            {{ slotProps.option?.email }} {{ slotProps.option?.phone_number }}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </template>
+
+                </auto-complete>
+              </icon-field>
+
+              <span class="fs-14px text-danger py-1 w-100 d-block"
+                v-if="errors?.email?.length">
+                {{ errors?.email[0] }}
+              </span>
+
+            </div>
+
           </div>
         </div>
 
@@ -210,6 +465,7 @@
           </div>
 
           <div class="modal-body p-0 pt-3">
+
             <div class="mb-3">
               <label class="mb-2 fs-16px text-head">
                 Lead title
@@ -231,6 +487,29 @@
               </span>
             </div>
 
+            <div class="mb-3">
+              <label class="mb-2 fs-16px text-head">
+                Source
+                <span class="text-soft fs-12px ms-1">(Optional)</span>
+              </label>
+
+              <icon-field>
+                <input-icon class="pi pi-pen-to-square fs-14px z-index-999" />
+                <auto-complete class="w-100 auto-complete-small"
+                  panel-class="auto-complete-option-small"
+                  input-class="w-100 auto-complete-input"
+                  dropdown-class="py-1"
+                  placeholder="Type lead source"
+                  @focus="delete errors?.lead_source"
+                  v-model="attributes.lead_source"
+                  :suggestions="filterSources"
+                  @complete="handleFilterSource" />
+              </icon-field>
+
+              <span class="fs-14px text-danger py-1 w-100 d-block"
+                v-if="errors?.lead_source?.length">{{ errors?.lead_source[0] }}</span>
+            </div>
+
             <div class="mb-3 position-relative">
               <label class="mb-2 fs-16px text-head">
                 Status
@@ -238,7 +517,7 @@
               </label>
 
               <select-option filter
-                v-model="attributes.lead_status"
+                v-model="attributes.select_status"
                 :options="leadStatuses"
                 :filterFields="['name']"
                 optionLabel="name"
@@ -302,7 +581,7 @@
                     <div class="owner-name fw--bold text-hard">
                       {{ slotProps.value.name || slotProps.value.email }}
                     </div>
-                    
+
                   </div>
 
                   <span v-else>
@@ -343,8 +622,8 @@
         <div class="col-lg-7 py-2">
           <div class="d-flex justify-content-between align-items-center">
             <div>
-              <Button @click="hideModal"
-                class="" size="small" severity="danger">Close</Button>
+              <button @click="hideModal"
+                class="btn btn-danger">Close</button>
             </div>
             <div>
               <loading-button :is-loading="isLoading"
@@ -364,7 +643,7 @@
     line-height: 16px;
   }
 
-  :deep(.input-lead-source) {
+  :deep(.auto-complete-input) {
     padding-left: 2.5rem !important;
   }
 
